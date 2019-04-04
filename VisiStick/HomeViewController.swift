9e1a512d17bb411d408f12c6ada4a399c4019eac
Seth Teichman <tei06398@stu.niskyschools.org>
2019-04-04 14:02:40 -0400

    Simple Navigation Hierarchy Created

---
+++ VisiStick/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  VisiStick
+//
+//  Created by PLTW Student on 4/4/19.
+//  Copyright © 2019 VisiStick. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func unwindToHome(segue: UIStoryboardSegue) {
+        
+    }
+}
