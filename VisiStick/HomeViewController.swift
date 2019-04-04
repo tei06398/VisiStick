ca98baeb38d1206f7488cffb7f21be277c91f0d4
Seth Teichman <tei06398@stu.niskyschools.org>
2019-04-04 14:34:49 -0400

    Added Dependency on CoreBluetooth TODO: Implement Bluetooth Scanner

--- VisiStick/HomeViewController.swift
+++ VisiStick/HomeViewController.swift
@@ -7,14 +7,9 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
