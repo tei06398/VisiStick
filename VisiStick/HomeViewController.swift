618852841ebeab73478a3657db9011f6b50ab3d6
Seth Teichman <tei06398@stu.niskyschools.org>
2019-04-08 14:45:16 -0400

    Work to get Beep Function ! NOT FUNCTIONAL BUILD

--- VisiStick/HomeViewController.swift
+++ VisiStick/HomeViewController.swift
@@ -8,32 +8,35 @@
 
 import UIKit
 import CoreBluetooth
+import AVFoundation
 
 class HomeViewController: UIViewController {
+    var audioPlayer = AVAudioPlayer()
+    //Crashes on First Run?
+    
+    var beep = Bundle.main.path(forResource: "beep", ofType: "mp3")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     @IBAction func unwindToHome(segue: UIStoryboardSegue) {
         
     }
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            let url = URL(fileURLWithPath: beep!)
+            audioPlayer.stop()
+            audioPlayer = try AVAudioPlayer(contentsOf: url )
+            audioPlayer.play()
+        }
+        catch{
+            print(error)
+        }
+    }
+
 }
