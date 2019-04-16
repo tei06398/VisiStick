a32ad1d978874b212a35ec2b4c4f041027370883
Seth <seth@macbook.local>
2019-04-16 13:14:32 -0400

    Fixed UI Constraints, Tested with Device - Data Reception works properly!

--- VisiStick/InfoViewController.swift
+++ VisiStick/InfoViewController.swift
@@ -15,7 +15,7 @@ class InfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.automaticallyAdjustsScrollViewInsets = false
         // Do any additional setup after loading the view.
     }
 
